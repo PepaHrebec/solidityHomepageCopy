2219ab1e8c2f0f7ae9ec90f199c52dee825834df
Josef Hrebec <josefhrebec02@gmail.com>
2023-03-21 12:46:50 +0100

    Add drop-down menu interact.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const btn = document.querySelector(".hdr-icon");
-const list = document.querySelector(".hdr-a-wrap");
+const list = document.querySelector(".hdr-a-wrap-hidden");
 
 btn.addEventListener("click", () => {
-  list.classList.contains("btn-clicked")
-    ? list.classList.remove("btn-clicked")
-    : list.classList.add("btn-clicked");
+  if (list.classList.contains("btn-clicked")) {
+    list.classList.add("hdr-a-wrap-hidden");
+    list.classList.remove("btn-clicked");
+  } else {
+    list.classList.add("btn-clicked");
+    list.classList.remove("hdr-a-wrap-hidden");
+  }
 });
